03e7bf2c5b5fe8e69602b7ce997080ec034afab6
Merlin Mason <info@merlinmason.co.uk>
2014-09-07 22:59:56 +0100

    style tweaks

--- single.php
+++ single.php
@@ -1,1 +1,1 @@
-<?php	if ( in_category(news) ) {		include('_news-single.php');	} elseif (in_category(gallery) ) {		include('_gallery-single.php');	} else {		include('404.php');	}?>
+<?php	if ( in_category(news) ) {		include('_news-single.php');	} elseif (in_category(custom) ) {		include('_custom-single.php');	} else {		include('404.php');	}?>
