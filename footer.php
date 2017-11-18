dbbcbe486af6725f3dd82178a58b7a29e58ebae8
Merlin Mason <info@merlinmason.co.uk>
2017-11-18 11:41:30 +0000

    remove odd formatting

--- footer.php
+++ footer.php
@@ -1,1 +1,13 @@
-</div> <!-- Close Page Wrap --><div id="social">	<ul>		<li><a target="_blank" class="icon twitter" href="https://twitter.com/donhoubicycles">Twitter</a></li>		<li><a target="_blank" class="icon facebook" href="https://www.facebook.com/pages/Donhou-Bicycles/202867809742131">Facebook</a></li>		<li><a target="_blank" class="icon instagram" href="http://instagram.com/donhoubicycles">Instagram</a></li>	</ul></div><?php wp_footer(); ?></body></html>
+</div> <!-- Close Page Wrap -->
+
+<div id="social">
+	<ul>
+		<li><a target="_blank" class="icon twitter" href="https://twitter.com/donhoubicycles">Twitter</a></li>
+		<li><a target="_blank" class="icon facebook" href="https://www.facebook.com/pages/Donhou-Bicycles/202867809742131">Facebook</a></li>
+		<li><a target="_blank" class="icon instagram" href="http://instagram.com/donhoubicycles">Instagram</a></li>
+	</ul>
+</div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php	if (is_home) {		// if home load news template and only show news posts		query_posts($query_string . '&cat=3');		include('category-news.php');	}?>
+<?php
+
+	if (is_home) {
+
+		// if home load news template and only show news posts
+		query_posts($query_string . '&cat=3');
+		include('category-news.php');
+
+	}
+
+?>
