730c000baac8eca3e3512d512b3d6f853c54bd96
Merlin Mason <info@merlinmason.co.uk>
2017-11-18 11:42:04 +0000

    Allow more DSS models to be created individually

--- signature-steel-intro.php
+++ signature-steel-intro.php
@@ -1,1 +1,30 @@
-<?php /* Template Name: Signature Steel Intro */ ?><?php get_header(); ?><div id="content">	<?php while ( have_posts() ) : the_post() ?>	<article class="post">		<a href="<?php the_field('dss1_link'); ?>">			<img class="fit mt-0" src="<?php the_field('dss1_image'); ?>" alt="<?php the_field('heading'); ?> DSS1">		</a>		<a href="<?php the_field('dss2_link'); ?>">			<img class="fit" src="<?php the_field('dss2_image'); ?>" alt="<?php the_field('heading'); ?> DSS2">		</a>		<h1 class="page-heading">			<?php the_field('heading'); ?>			<br>			—		</h1>		<p>			<?php the_field('intro'); ?>		</p>	</article>  <?php endwhile; ?></div><?php get_footer(); ?>
+<?php /* Template Name: Signature Steel Intro */ ?>
+<?php get_header(); ?>
+<div id="content">
+	<?php while ( have_posts() ) : the_post() ?>
+
+		<article class="post">
+			<h1 class="page-heading">
+				<?php the_field('heading'); ?>
+				<br>
+				—
+			</h1>
+
+			<p><?php the_field('intro'); ?></p>
+			
+			<?php // links to all sub pages
+				if(get_field('dss')):
+					while(the_repeater_field('dss')):
+						?>
+							<a href="<?php the_sub_field('link'); ?>">
+								<img class="fit mt-0" src="<?php the_sub_field('image'); ?>" alt="<?php the_field('heading'); ?>">
+							</a>
+						<?php
+					endwhile;
+				endif;
+			?>
+		</article>
+
+	<?php endwhile; ?>
+</div>
+<?php get_footer(); ?>
